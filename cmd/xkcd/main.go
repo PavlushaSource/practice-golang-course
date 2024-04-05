77aca399c30a588c6b52d81420ec42a4dd2922d5
PavlushaSource <pavlushkin.dima1785@gmail.com>
2024-04-05 21:09:01 +0300

    feat: Add xkcd client functions

--- cmd/xkcd/main.go
+++ cmd/xkcd/main.go
@@ -3,6 +3,9 @@ package main
 import (
 	"fmt"
 	"github.com/PavlushaSource/yadro-practice-course/pkg/config"
+	"github.com/PavlushaSource/yadro-practice-course/pkg/database"
+	"github.com/PavlushaSource/yadro-practice-course/pkg/logger"
+	"github.com/PavlushaSource/yadro-practice-course/pkg/xkcd"
 	"os"
 )
 
@@ -14,5 +17,38 @@ func main() {
 		os.Exit(1)
 	}
 
-	fmt.Println(cfg.Env)
+	// init logger
+	log, err := logger.SetupLogger(cfg.Env)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	// init storage
+	log.Debug("Init database", "db_file", cfg.DbFile)
+
+	storage, cancel, err := database.NewJsonStorage(cfg.DbFile)
+	defer cancel()
+	if err != nil {
+		log.Error(err.Error())
+		os.Exit(1)
+	}
+
+	//init client
+	log.Debug("Init client")
+	client := xkcd.NewClient()
+
+	// read site
+	log.Debug("Reading all comics from site")
+	comics := xkcd.GetComics(client, cfg.SourceUrl, log)
+	if comics == nil {
+		log.Error("Cannot read all comics from site")
+		os.Exit(1)
+	}
+
+	for k := range comics {
+		fmt.Println(k)
+	}
+	_ = storage
+
 }
---
+++ pkg/logger/logger.go
@@ -0,0 +1,29 @@
+package logger
+
+import (
+	"fmt"
+	"log/slog"
+	"os"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
+)
+
+func SetupLogger(env string) (*slog.Logger, error) {
+	var log *slog.Logger
+	switch env {
+	case envLocal:
+		log = slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	case envDev:
+		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	case envProd:
+		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}))
+	default:
+		return nil, fmt.Errorf("invalid env: %s", env)
+	}
+
+	return log, nil
+}
